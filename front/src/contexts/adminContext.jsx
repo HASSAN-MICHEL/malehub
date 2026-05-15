539f4fd199ca901f07e575a951a0124de2fc9673
Votre Nom <votre-email-vercel@exemple.com>
2026-05-15 13:52:23 +0100

    front build auth admin

--- front/src/contexts/adminContext.jsx
+++ front/src/contexts/adminContext.jsx
@@ -74,7 +74,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://maleahub.vercel.app/api';
 
 const AdminAuthContext = createContext(null);
 
